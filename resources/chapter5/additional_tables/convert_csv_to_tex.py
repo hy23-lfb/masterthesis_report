d52f7b14db33da39d13af4d5b39dbd2e0e1591ae
hy23 <imharshay@gmail.com>
2023-01-08 00:24:09 +0100

    Need to add life to tables.

---
+++ resources/chapter5/additional_tables/convert_csv_to_tex.py
@@ -0,0 +1,21 @@
+import csv
+from tabulate import tabulate
+
+# Open the CSV file and read in the data
+with open(r'D:\04.GitRepository\masterthesis_report\resources\chapter5\additional_tables\vanilla\vanilla.csv', 'r') as f:
+  data = list(csv.reader(f))
+
+'''
+# Make the first row and first column bold
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        if i == 0 or j == 0:
+            data[i][j] = "{" + data[i][j] + '}'
+'''
+
+# Generate the LaTeX table
+table = tabulate(data, tablefmt='latex')
+
+# Open the LaTeX file for writing
+with open(r'D:\04.GitRepository\masterthesis_report\resources\chapter5\additional_tables\vanilla.tex', 'w') as f:
+    f.write(table)
